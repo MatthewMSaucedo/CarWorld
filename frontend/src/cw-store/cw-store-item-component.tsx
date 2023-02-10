22ac073335c44eba40fefbb423cc7d67aa48c1b6
matthew saucedo <themattsaucedo@gmail.com>
2023-02-09 21:31:49 -0500

    add SSM, AWS client stuff

--- frontend/src/cw-store/cw-store-item-component.tsx
+++ frontend/src/cw-store/cw-store-item-component.tsx
@@ -1,10 +1,9 @@
 import '../App.scss'
 import './cw-store.scss'
-import React, { useState } from "react";
+import { useState } from "react";
 import { CWShoppingItemType, CWStoreItem } from './cw-store-item'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
 import { CWShoppingCart } from './cw-shopping-cart';
 
 function CWStoreItemComponent(cwStoreItem: CWStoreItem, cwShoppingCart: CWShoppingCart, setCwShoppingCart: any) {
@@ -15,21 +14,19 @@ function CWStoreItemComponent(cwStoreItem: CWStoreItem, cwShoppingCart: CWShoppi
 
     const [value, setValue] = useState(getInitialState);
 
+    // TODO: type
     const handleChange = (e: any) => {
         setValue(e.target.value);
     };
 
+    // TODO: type
     const addToCart = (clickEvent: any) => {
         console.log(clickEvent)
         console.log(`size: ${value}`)
         if (cwStoreItem.type === CWShoppingItemType["Clothing"]) {
-            console.log("crowes")
-            console.log(`cwStoreItem size: ${cwStoreItem.title}`)
             cwShoppingCart.add({cwStoreItem: cwStoreItem, size: value})
         }
         setCwShoppingCart(cwShoppingCart)
-        console.log(cwShoppingCart.contents[0]?.size)
-        console.log(cwShoppingCart.size)
     }
 
     const sizeForm = (showSize: boolean) => {
@@ -45,10 +42,13 @@ function CWStoreItemComponent(cwStoreItem: CWStoreItem, cwShoppingCart: CWShoppi
         }
    }
 
-    //  CW-TODO: Add input takers for size, color etc
   return (
     <div className="cw-store-item">
-        <img className="item-image" src={process.env.PUBLIC_URL + cwStoreItem.image} />
+        <img
+            className="item-image"
+            src={ process.env.PUBLIC_URL + cwStoreItem.image }
+            alt="This is a very cool item you would love to own"
+        />
         <div className="content">
             {cwStoreItem.title}
         </div>
@@ -56,6 +56,7 @@ function CWStoreItemComponent(cwStoreItem: CWStoreItem, cwShoppingCart: CWShoppi
             ${cwStoreItem.price}
         </div>
         { sizeForm(cwStoreItem.type === CWShoppingItemType["Clothing"]) }
+        { /* TODO: Add logic to disable if size is still "Size" */ }
         <Button as="a" variant="primary" onClick={ (clickEvent) => addToCart(clickEvent) }>
             Add to Cart
         </Button>
