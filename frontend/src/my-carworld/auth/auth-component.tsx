3949344e19e651a472f7f698220b1f78e62f225c
matthew saucedo <themattsaucedo@gmail.com>
2023-08-07 22:33:11 -0400

    navbar changes

--- frontend/src/my-carworld/auth/auth-component.tsx
+++ frontend/src/my-carworld/auth/auth-component.tsx
@@ -14,37 +14,60 @@ type Inputs = {
 
 function CWAuthComponent() {
     // State-managed auth form toggle - Register or Login
-    const [showRegister, setShowRegister] = useState(true);
+    /* const [showRegister, setShowRegister] = useState(true); */
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<Inputs>()
+    /* const { register } = useForm<Inputs>() */
+    /* const { handleSubmit } = useForm<Inputs>() */
 
     // Form submission
     const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+    const showRegister = true
 
     const registerForm = () => {
         return (
         <div className="Auth-form-container">
-            <form className="Auth-form-register" onSubmit={handleSubmit(onSubmit)}>
+            <form className="Auth-form-register" >
                 <div className="Auth-form-content">
                     <div className="Auth-form-title">
                         Register
                     </div>
-                    <label>Example</label>
-                    <input {...register("example")} defaultValue="test" />
-                    <label>ExampleRequired</label>
-                    <input
-                        {...register("exampleRequired", { required: true, maxLength: 10 })}
-                    />
-                    {errors.exampleRequired && <p>This field is required</p>}
-                    <input type="submit" />
+                    <div className="text-center">
+                        Already registered?
+                        <p className="link-primary" >
+                        Sign in!
+                        </p>
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>
+                            Email address
+                        </label>
+                        <input
+                        type="email"
+                        className="form-control mt-1"
+                        placeholder="Email Address"
+
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Full Name</label>
+                        <input
+                        type="username"
+                        className="form-control mt-1"
+                        placeholder="e.g Jane Doe"
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Password</label>
+                        <input
+
+                        type="password"
+                        className="form-control mt-1"
+                        placeholder="Password"
+                        />
+                    </div>
+
                     <input type="checkbox" id="devote" name="devote" />
                     <label>I hereby acknowledge my Devotion to William Banks</label>
-
-                    {errors.exampleRequired && <p>This field is required</p>}
 
                     <div className="d-grid gap-2 mt-3">
                         <button type="submit" className="btn btn-primary">
@@ -65,7 +88,7 @@ function CWAuthComponent() {
                     <div className="Auth-form-title">Sign In</div>
                     <div className="text-center">
                         Not registered yet?{" "}
-                        <span className="link-primary" onClick={ () => setShowRegister(true)}>
+                        <span className="link-primary" >
                             Sign Up
                         </span>
                         </div>
