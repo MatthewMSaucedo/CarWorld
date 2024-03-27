ccfa9839ad90008973555b1e1757fa407e6956c4
Sam Zofkie <samzofkie@gmail.com>
2024-03-27 10:53:01 -0500

    (BE - Infrastructure): Merged backend/infra directory into backend/, updated .gitignore and backend/.gitignore to ignore commerce lambda layers, and demoved stripe-layer and powertools-layer code from the repo.

--- backend/infra/lambda/commerce/powertools-layer/python/lib/python3.7/site-packages/aws_lambda_powertools/metrics/provider/__init__.py
+++
@@ -1,5 +0,0 @@
-from aws_lambda_powertools.metrics.provider.base import BaseProvider
-
-__all__ = [
-    "BaseProvider",
-]
--- backend/infra/lambda/commerce/powertools-layer/python/lib/python3.7/site-packages/aws_lambda_powertools/utilities/parameters/types.py
+++
@@ -1,3 +0,0 @@
-from aws_lambda_powertools.shared.types import Literal
-
-TransformOptions = Literal["json", "binary", "auto", None]
